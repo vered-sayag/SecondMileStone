ebcf9be02c561a31af40f2cd7ccc5db7fbb2b984
vered-sayag <vered8765@gmail.com>
2019-01-15 17:33:57 +0200

    part c first fixsd

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include "MyParallelServer.h"
 #include "SearchSolver.h"
 #include "BestFirstSearcher.h"
+
 int main(int numArg, char *args[]) {
 
 //server_side::boot::Main::main(numArg,args);
-    Server *server = new MyParallelServer();
-    server->open(atoi(args[1]),new MyClientHandler(new SearchSolver(),new FileCacheManager("test3.txt")));
-    this_thread::sleep_for(chrono::milliseconds(10000));
-    server->stop();
-    delete (server);
+    if (numArg == 2 && regex_match(args[1], regex("^\\d+"))) {
+        Server *server = new MyParallelServer();
+        server->open(atoi(args[1]), new MyClientHandler(new SearchSolver(), new FileCacheManager("test3.txt")));
+        this_thread::sleep_for(chrono::milliseconds(10000));
+        server->stop();
+        delete (server);
+    } else {
+        throw invalid_argument("Invalid main arguments");
+    }
     return 0;
 }
